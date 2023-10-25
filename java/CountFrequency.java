b7a1a8de60311ac3de05df1d32b68acd7adc9a1f
Harsh <148313480+harsh24122004@users.noreply.github.com>
2023-10-25 20:42:07 +0530

    Delete java directory

--- java/CountFrequency.java
+++
@@ -1,9 +0,0 @@
-public class CountFrequency{
-
-    public static void main(String[] args) {
-        int arr[]={1,1,2,3,4,2,1};
-        HashMap<Integer , Integer> map=newHashMap<>();
-        map.put(key:1,value:1);
-        
-    }
-}
--- java/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test 
-{
-    public static void main(String[] args) {
-        int x = System.out.println("ABC");
-        System.out.println(x);
-    }    
-}
